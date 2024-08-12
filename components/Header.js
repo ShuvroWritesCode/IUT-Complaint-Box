e1fdbf896a4fe2bc145350f63eada71f32b7cc78
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2024-08-12 20:40:02 +0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,37 @@
-import { PlusCircleIcon, PencilSquareIcon, ListBulletIcon } from '@heroicons/react/24/outline';
+import { PlusCircleIcon, ListBulletIcon } from '@heroicons/react/24/outline';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Header() {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
   return (
-    <header /* className="bg-gradient-to-tr from-red-900 text-white to-red-600 py-5 shadow-md"*/>
-      <div className=" container mx-auto my-auto flex items-center justify-between">
-        <a className="font-serif text-xl text-red-normal tracking-tight" href="/" rel="ugc">
+    <header>
+      <div className="container mx-auto flex items-center justify-between">
+        <a className="font-serif text-xl text-red-pastel tracking-tight" href="/" rel="ugc">
           IUT Complaint Box
         </a>
         <nav>
-          <ul className="flex items-center space-x-2">
+          <ul className="flex items-center space-x-3">
             <li>
               <a
-                className="text-md px-3 py-3 rounded-lg hover:bg-red-pastel hover:text-white hover:border hover:border-white transition-colors duration-300 ease-in-out flex items-center space-x-1 group"
+                className="text-md px-2 py-2 rounded-lg border-2 border-red-pastel hover:bg-red-pastel hover:text-white hover:border hover:border-white transition-colors duration-300 ease-in-out flex items-center space-x-1 group"
                 href="/" rel="ugc"
               >
-                <PlusCircleIcon className="h-5 w-5 text-red-normal group-hover:text-white" />
-                <span className="font-serif ml-2 transition-opacity duration-200">Post</span>
+                <PlusCircleIcon className="h-5 w-5 text-red-800 group-hover:text-white" />
+                {!isMobile && (
+                  <span className="font-serif ml-2 transition-opacity duration-200">Post</span>
+                )}
               </a>
             </li>
             <li>
               <a
-                className="text-md px-3 py-3 rounded-lg hover:bg-red-pastel hover:text-white hover:border hover:border-white transition-colors duration-300 ease-in-out flex items-center space-x-1 group"
+                className="text-md px-2 py-2 rounded-lg border-2 border-red-pastel hover:bg-red-pastel hover:text-white hover:border hover:border-white transition-colors duration-300 ease-in-out flex items-center space-x-1 group"
                 href="/view" rel="ugc"
               >
-                <ListBulletIcon className="h-5 w-5 text-red-normal group-hover:text-white" />
-                <span className="font-serif ml-2 transition-opacity duration-200">View</span>
+                <ListBulletIcon className="h-5 w-5 text-red-800 group-hover:text-white" />
+                {!isMobile && (
+                  <span className="font-serif ml-2 transition-opacity duration-200">View</span>
+                )}
               </a>
             </li>
           </ul>
