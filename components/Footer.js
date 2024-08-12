b1b57cf87a51cce33da508e16007860cd5b7c874
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2024-08-12 20:39:40 +0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,26 +1,34 @@
 import { HomeIcon, CheckBadgeIcon } from '@heroicons/react/24/outline';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Footer() {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
   return (
     <footer className="py-10">
-      <div className="container mx-auto my-auto flex items-center justify-between">
-      <div className="font-serif text-sm text-red-normal tracking-tight">
+      <div className="container mx-auto flex items-center justify-between">
+      <div className="font-serif text-xs text-red-normal tracking-tight">
+          <p>IUT Complaint Box - v1.0 | All Right Deserved by IUT 20</p>
           <p>2024 @ Developed and maintained by IUT Batch 20</p>
         </div>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-3">
           <a
-            className="text-md px-3 py-3 rounded-lg hover:bg-red-pastel hover:text-white hover:border hover:border-white transition-colors duration-300 ease-in-out flex items-center space-x-1 group"
-            href="/" rel="ugc"
+            className="text-md px-2 py-2 rounded-lg border-2 border-red-pastel hover:bg-red-pastel hover:text-white hover:border hover:border-white transition-colors duration-300 ease-in-out flex items-center space-x-1 group"
+                href="/" rel="ugc"
           >
             <HomeIcon className="h-5 w-5 text-red-normal group-hover:text-white" />
-            <span className="font-serif ml-2 transition-opacity duration-200">Home</span>
+            {!isMobile && (
+                <span className="font-serif ml-2 transition-opacity duration-200">Home</span>
+            )}
           </a>
           <a
-            className="text-md px-3 py-3 rounded-lg hover:bg-red-pastel hover:text-white hover:border hover:border-white transition-colors duration-300 ease-in-out flex items-center space-x-1 group"
+            className="text-md px-2 py-2 rounded-lg border-2 border-red-pastel hover:bg-red-pastel hover:text-white hover:border hover:border-white transition-colors duration-300 ease-in-out flex items-center space-x-1 group"
             href="/policy" rel="ugc"
           >
             <CheckBadgeIcon className="h-5 w-5 text-red-normal group-hover:text-white" />
-            <span className="font-serif ml-2 transition-opacity duration-200">Policy</span>
+            {!isMobile && (
+              <span className="font-serif ml-2 transition-opacity duration-200">Policy</span>
+            )}
           </a>
         </div>
       </div>
