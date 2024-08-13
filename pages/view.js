07ee6bd88886f546e468d2ffd4b366151b81e528
Iftekharul Haque Shuvro <shuvrohawk@gmail.com>
2024-08-13 14:03:01 +0600

    Update view.js

--- pages/view.js
+++ pages/view.js
@@ -5,7 +5,7 @@ import Footer from '../components/Footer';
 
 export default function View() {
   return (
-    <>
+    < >
       <Head>
         <title>View - IUT Complaint Box</title>
       </Head>
