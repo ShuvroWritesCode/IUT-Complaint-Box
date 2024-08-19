28840d450277609144cf6385f33fe09eadc7be25
Iftekharul Haque Shuvro <shuvrohawk@gmail.com>
2024-08-19 09:45:27 +0600

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "26ea890463dedf9f"; }
+/******/ 		__webpack_require__.h = function() { return "7c233519bec05ff6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
